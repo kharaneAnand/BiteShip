2e463fa304dec50d5dddee36d15a8b4ffb8b5037
Anand Kharane <anandkharane@gmail.com>
2026-02-28 02:49:06 +0530

    feat(auth): add user model and MongoDB connection setup

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express' 
 import dotenv from 'dotenv' 
+import connectDB from './config/db.js';
 
 dotenv.config() ;
 
@@ -9,4 +10,5 @@ const PORT = process.env.PORT || 5000 ;
 
 app.listen( PORT , ()=>{
    console.log(`Auth service is running on port ${PORT}`) ;
+   connectDB() ;
 }) ;
