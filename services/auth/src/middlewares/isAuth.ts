afbbb5a220869c56d86a4b320c5c5055319f8368
Anand Kharane <anandkharane@gmail.com>
2026-03-01 01:33:47 +0530

    feat(auth) :- implementing the myProfile API

--- services/auth/src/middlewares/isAuth.ts
+++ services/auth/src/middlewares/isAuth.ts
@@ -47,37 +47,3 @@ export const isAuth = async(req:AuthenticatedRequest , res:Response , next:NextF
 };
 
 
-const allowedRoles = ["customer" , "rider" , "seller"] as const ;
-type Role = (typeof allowedRoles)[number];
-
-export const addUserRole = TryCatch(async(req:AuthenticatedRequest , res )=>{
-    if(!req.user?._id){
-        return res.status(401).json({
-            message :"Unautherized" ,
-        });
-    }
-
-    
-    const {role} = req.body as {role : Role} ;
-    if(!allowedRoles.includes(role)){
-        return res.status(400).json({
-            message :"Invalid role" ,
-        });
-    }
-
-    const user = await User.findByIdAndUpdate(req.user._id , {role} , {new:true}) 
-
-    if(!user){
-        return res.status(404).json({
-            message:"User not found" ,
-        });
-    }
-
-    const token = jwt.sign({user} , process.env.JWT_SEC as string , {
-        expiresIn:"15d",
-     });
-
-     res.json({user , token })
-});
-
-
--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,10 +1,11 @@
 import express from 'express' ;
-import { loginUser } from '../controllers/auth.js';
-import { addUserRole, isAuth } from '../middlewares/isAuth.js';
+import { loginUser  , addUserRole, myProfile } from '../controllers/auth.js';
+import { isAuth } from '../middlewares/isAuth.js';
 
 const router = express.Router() ;
 
 router.post("/login" , loginUser) ;
 router.put("/add/role" , isAuth , addUserRole) ;
+router.get("/me" , isAuth , myProfile) ;
 
 export default router ;
