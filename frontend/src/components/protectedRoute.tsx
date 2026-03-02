9c915a39bc35ea7ba305b9dfaeae7412c2a07a62
Anand Kharane <anandkharane@gmail.com>
2026-03-03 01:04:29 +0530

    feat(frontend): implement ProtectedRoute and PublicRoute with role-based auth guards

---
+++ frontend/src/components/protectedRoute.tsx
@@ -0,0 +1,25 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAppData } from "../context/AppContext"
+
+const ProtectedRoute = () =>{
+    const {isAuth , user , loading} = useAppData() 
+
+    if(loading) return null ;
+
+    const location = useLocation() 
+    if(!isAuth){
+        return <Navigate to={"/login"} replace /> ;
+    }
+
+    if(user?.role === null && location.pathname !== "/select-role"){
+        return <Navigate to={"/select-role"} replace /> ;
+    }
+
+    if(user?.role !== null && location.pathname === "/select-role"){
+        return <Navigate to={"/"} replace /> ;
+    }
+
+    return <Outlet />
+};
+
+export default ProtectedRoute ;
