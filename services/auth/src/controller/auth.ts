b3e11f9c8c0bbdff3775a90391be9cdf73eb6cc5
Anand Kharane <anandkharane@gmail.com>
2026-05-07 17:36:53 +0530

    created a TryCatch middleware insted of using it evberyever to wrote that code , using as a middleware

--- services/auth/src/controller/auth.ts
+++ services/auth/src/controller/auth.ts
@@ -1,5 +1,28 @@
-import { Request , Response } from "express"
+import User from "../model/User.js";
+import jwt from "jsonwebtoken";
+import TryCatch from "../middleware/trycatch.js";
 
-export const loginUser = async(req:Request , res:Response)=>{
-   res.json(req.body) ;
-}
+export const loginUser = TryCatch(async(req , res)=>{
+
+     const {email , name , picture } = req.body() ;
+
+        let user = await User.findOne({email}) 
+
+        if(!user){
+            user = await User.create({
+                name , 
+                email ,
+                image : picture ,
+            });
+        }
+
+        const token = jwt.sign({user} , process.env.JWT_SEC as string , {
+            expiresIn:"15d" ,
+        });
+
+        res.status(200).json({
+            message : "LogIn Success" ,
+            token , 
+            user ,
+        });
+})
