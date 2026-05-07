74f25edf52f207ecdbd284a1c72f1d732802b294
Anand Kharane <anandkharane@gmail.com>
2026-05-07 16:59:55 +0530

    setting the structure of the project and strating with the authentication service

---
+++ services/auth/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express' ;
+import dotenv from 'dotenv' ;
+import connectDB from './config/db.js';
+import authRouter from './routes/auth.js'
+
+dotenv.config() ;
+const app = express() ;
+
+app.use("/api/auth" , authRouter) ;
+
+const PORT = process.env.PORT || 5000 ;
+
+app.listen(PORT , ()=>{
+    console.log(`Auth Service is running on the ${PORT}`) ;
+    connectDB();
+});
+
