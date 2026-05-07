cf14fd496cea0459b04bc25dba1636d79f120832
Anand Kharane <anandkharane@gmail.com>
2026-05-07 18:21:52 +0530

    feat(Auth) :- add user api endpoint and controller function created

--- services/auth/src/middleware/isAUth.ts
+++ services/auth/src/middleware/isAUth.ts
@@ -1,6 +1,7 @@
 import { Request , Response , NextFunction } from "express";
 import jwt , {JwtPayload} from 'jsonwebtoken'
-import { IUser } from "../model/User.js";
+import User, { IUser } from "../model/User.js";
+import TryCatch from "./trycatch.js";
 
 export interface AuthenticatedRequest extends Request{
     user?:IUser | null ;
@@ -44,3 +45,39 @@ export const isAuth = async(req:AuthenticatedRequest , res:Response , next:NextF
         })
     }
 };
+
+
+const allowedRoles = ["customer" , "rider" , "seller"] as const ;
+type Role = (typeof allowedRoles)[number] ;
+
+export const addUserRole = TryCatch(async(req:AuthenticatedRequest , res)=>{
+    
+    if(!req.user?._id){
+        return res.status(401).json({
+            message :"Unathorized" ,
+        }) ;
+    }
+
+    const {role} = req.body as {role:Role};
+    
+    if(!allowedRoles.includes(role)){
+        return res.status(400).json({
+            message:"Invalid role" , 
+        });
+    }
+
+    const user = await User.findByIdAndUpdate(req.user._id , {role} , {new:true}) 
+
+    if(!user){
+        return res.status(404).json({
+            message:"User not found" ,
+        });
+    }
+
+    const token = jwt.sign({user}, process.env.JWT_SEC as string ,{
+        expiresIn : "15d" ,
+    });
+
+    res.json({user , token }) ;
+
+}) ;
--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,7 +1,9 @@
 import express from 'express' ;
 import { loginUser } from '../controller/auth.js';
+import { addUserRole, isAuth } from '../middleware/isAUth.js';
 
 const router = express.Router() ;
 router.post("/login" , loginUser) ;
+router.put("/add/role" , isAuth , addUserRole) ;
 
 export default router ;
