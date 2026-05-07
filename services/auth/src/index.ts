f56874ce8122e10e275e5ba3e652382679e9491f
Anand Kharane <anandkharane@gmail.com>
2026-05-07 17:22:43 +0530

    setting the routes and controller for the auth service , inste of taking the request from frontend tried with controller as we are creating a backend first

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -6,6 +6,8 @@ import authRouter from './routes/auth.js'
 dotenv.config() ;
 const app = express() ;
 
+
+app.use(express.json())
 app.use("/api/auth" , authRouter) ;
 
 const PORT = process.env.PORT || 5000 ;
--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import express from 'express' ;
+import { loginUser } from '../controller/auth.js';
 
 const router = express.Router() ;
-
+router.post("/login" , loginUser) ;
 
 export default router ;
