6228595f09bc5a21392e078941324f4d332bf91c
Anand Kharane <anandkharane@gmail.com>
2026-05-09 00:40:59 +0530

    restaurant :- created a deleteMenuItem and toggleMenuItemAvailablity in controller and setup the routes

--- services/restaurant/src/routes/menuitem.ts
+++ services/restaurant/src/routes/menuitem.ts
@@ -1,11 +1,13 @@
 import express from 'express' ;
 import { isAuth, isSeller } from '../middleware/isAuth.js';
-import { addMenuItem } from '../controllers/menuitem.js';
+import { addMenuItem, deleteMenuItem, toggleMenuItemAvailability } from '../controllers/menuitem.js';
 
 
 const router = express.Router() 
 
 router.post("/new" , isAuth , isSeller , addMenuItem) ;
 router.get("/all/:id" , isAuth , addMenuItem) ;
+router.delete("/:id" , isAuth , isSeller , deleteMenuItem) ;
+router.put("/status/:id" , isAuth , isSeller , toggleMenuItemAvailability) ;
 
 export default router ;
