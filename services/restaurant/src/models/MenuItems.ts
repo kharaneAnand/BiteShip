bdf3c1e338bef9e1a2e9ad071bc1f037e096935e
Anand Kharane <anandkharane@gmail.com>
2026-05-08 23:13:56 +0530

    restaurant :- created a controller function for the addMenuItems and complte the api

---
+++ services/restaurant/src/models/MenuItems.ts
@@ -0,0 +1,47 @@
+import mongoose, { Document,  Schema } from "mongoose";
+
+export interface IMenuItem extends Document{
+    restaurantId : mongoose.Types.ObjectId ;
+    name : string ;
+    description : string ;
+    image?:string ;
+    price:number ;
+    isAvailable:boolean ;
+    createAt : Date;
+    updatedAt:Date ;
+}
+
+const schema = new Schema<IMenuItem>({
+    restaurantId:{
+        type:Schema.Types.ObjectId ,
+        ref:"Restaurant" ,
+        required:true ,
+        index:true ,
+    },
+    name : {
+        type:String ,
+        trim : true,
+        required:true , 
+    },
+     description : {
+        type:String ,
+        trim : true, 
+    },
+     price : {
+        type:Number ,
+        required:true , 
+    },
+     image : {
+        type:String ,
+        required:true , 
+    },
+    isAvailable:{
+        type:Boolean ,
+        required:true ,
+    },
+},
+{
+    timestamps:true ,
+});
+
+export default mongoose.model<IMenuItem>("MenuItem" , schema) ;
