121d80f1fb545c45c7bd021b26742c1a261c4a17
Anand Kharane <anandkharane@gmail.com>
2026-05-13 21:38:28 +0530

    created  a socketCOntext and wrap the app within the socketProvider and created a socket room and in that room user and restaurant are there

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,20 @@ import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { AppProvider } from './context/AppContext.tsx'
 import 'leaflet/dist/leaflet.css';
+import { SocketProvider } from './context/SocketContext.tsx'
 
 export const authService = "http://localhost:5000";
 export const restaurantService = "http://localhost:5001";
 export const utilsService = "http://localhost:5002";
+export const realtimeService = "http://localhost:5003";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId='898610648717-j9f9r3mv9vru0im13q0teo4ahcrja6d9.apps.googleusercontent.com'>
       <AppProvider>
-         <App />
+        <SocketProvider>
+           <App />
+        </SocketProvider>
       </AppProvider>
     </GoogleOAuthProvider>
   </StrictMode>,
