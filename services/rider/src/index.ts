415195a1b7e41fa07fdd532edf01c4ed1e8f469e
Anand Kharane <anandkharane@gmail.com>
2026-05-14 23:25:55 +0530

    created a rider controller at rider service

--- services/rider/src/index.ts
+++ services/rider/src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express' ;
 import dotenv from 'dotenv' ;
+import connectDB from './config/db.js';
 
 dotenv.config() ;
 
 const app = express() ;
+
 const PORT = process.env.PORT || 5004 ;
 
-
-app.listen(PORT ,()=>{
-    console.log(`Rider service is running on ${PORT}`) ;
-});
-
+app.listen(PORT , ()=>{
+    console.log(`Rider service is running on the ${PORT}`) ;
+    connectDB() ;
+})
