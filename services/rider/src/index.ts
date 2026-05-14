49d8a8e1335d7e70bd25f9567a647167fbd35adf
Anand Kharane <anandkharane@gmail.com>
2026-05-14 22:51:33 +0530

    Rider service setup is done

---
+++ services/rider/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express' ;
+import dotenv from 'dotenv' ;
+
+dotenv.config() ;
+
+const app = express() ;
+const PORT = process.env.PORT || 5004 ;
+
+
+app.listen(PORT ,()=>{
+    console.log(`Rider service is running on ${PORT}`) ;
+});
+
