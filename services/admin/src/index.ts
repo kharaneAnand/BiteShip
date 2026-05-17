d4793c4bfa80568f6dbcd47a963d0cb745c863ec
Anand Kharane <anandkharane@gmail.com>
2026-05-17 23:26:58 +0530

    for fetching a restaurant and rider which are not verified and to mark as verified created a controller and added the routes for them also

--- services/admin/src/index.ts
+++ services/admin/src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express' ;
 import dotenv from 'dotenv' ;
+import adminRouter from './routes/admin.js'
+import cors from 'cors' ;
 
 dotenv.config() ;
 const app = express() ;
 
+app.use(cors()) ;
+app.use('/api/v1' , adminRouter) ;
+
 const PORT = process.env.PORT || 5005 ;
+
 
 app.listen(PORT , ()=>{
     console.log(`Admin service is running on Port ${process.env.PORT}`) ;
