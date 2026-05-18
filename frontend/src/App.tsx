1f301d801f91248db927b62107bb5280e3121c6c
Anand Kharane <anandkharane@gmail.com>
2026-05-18 21:33:55 +0530

    Fix import path for Login component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import Home from './pages/Home'
-import Login from './pages/Login'
+import Login from './pages/login'
 import {Toaster} from 'react-hot-toast'
 import ProtectedRoute from './components/protetecRoute'
 import PublicRoute from './components/publicRoute'
